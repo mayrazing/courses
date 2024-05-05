a9e036d5f504a99815b68c367c3c1175828dfe4d
mayrazing <zengmeiyan0822@outlook.com>
2024-05-06 01:11:25 +0300

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -19,7 +19,6 @@ function createApp(database) {
     const type = req.query.type;
     const baseCost = database.findBasePriceByType(type).cost;
     const date = parseDate(req.query.date);
-    const date2 = parsePlainDate(req.query.date);
     const cost = calculateCost(age, type, date, baseCost);
     res.json({ cost });
   });
@@ -30,7 +29,6 @@ function createApp(database) {
     }
   }
 
-  const parsePlainDate = dateString => dateString ? Temporal.PlainDate.from(dateString) : undefined;
 
   function calculateCost(age, type, date, baseCost) {
     if (type === "night") {
