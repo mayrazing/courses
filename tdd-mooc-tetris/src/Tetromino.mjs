201238985cd1010bebf77b68def2fcbeadbffaa1
mayrazing <zengmeiyan0822@outlook.com>
2024-05-07 23:37:15 +0300

    level-3: refactor pass

--- tdd-mooc-tetris/src/Tetromino.mjs
+++ tdd-mooc-tetris/src/Tetromino.mjs
@@ -5,13 +5,11 @@ export class Tetromino {
     static T_SHAPE = new Tetromino(".T.\nTTT\n...\n", 4);
     static I_SHAPE = new Tetromino(".....\n.....\nIIII.\n.....\n.....\n", 2);
 
-    shape;
     orientationShapes = [];
     currentOrientation;
 
     constructor(s, orientationCount=0) {
-        this.shape = RotatingShape.fromString(s);
-        this.orientationShapes[0] = this.shape;
+        this.orientationShapes[0] = RotatingShape.fromString(s);
         if (orientationCount > 0) {
             for (let i = 1; i < orientationCount; i++) {
                 this.orientationShapes[i] = this.orientationShapes[i-1].rotateRight();
@@ -25,17 +23,17 @@ export class Tetromino {
     }
 
     rotateRight() {
-        return this.getRotatingTetromino(this.shape.rotateRight(), this.orientationShapes, this.currentOrientation + 1);
+        return this.getRotatingTetromino(this.orientationShapes, this.currentOrientation, 1);
     }
 
     rotateLeft() {
-        return this.getRotatingTetromino(this.shape.rotateLeft(), this.orientationShapes, this.currentOrientation - 1);
+        return this.getRotatingTetromino(this.orientationShapes, this.currentOrientation, -1);
     }
 
-    getRotatingTetromino(shape, orientations, current) {
-        let tetromino = new Tetromino(shape.toString());
+    getRotatingTetromino(orientations, current, nextStep) {
+        let tetromino = new Tetromino(orientations[current].toString());
         tetromino.orientationShapes = orientations;
-        tetromino.currentOrientation = (current + orientations.length) % orientations.length;
+        tetromino.currentOrientation = (current + nextStep + orientations.length) % orientations.length;
         return tetromino;
     }
 }
