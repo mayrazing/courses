7fc93e2f61217f26757e07aa86dcf185278f122a
mayrazing <zengmeiyan0822@outlook.com>
2024-05-07 23:26:11 +0300

    level-3: refactor pass

--- tdd-mooc-tetris/src/Tetromino.mjs
+++ tdd-mooc-tetris/src/Tetromino.mjs
@@ -2,13 +2,20 @@ import { RotatingShape } from "./RotatingShape.mjs";
 
 export class Tetromino {
 
-    static T_SHAPE = new Tetromino(".T.\nTTT\n...\n");
-    static I_SHAPE = new Tetromino(".....\n.....\nIIII.\n.....\n.....\n");
+    static T_SHAPE = new Tetromino(".T.\nTTT\n...\n", 4);
+    static I_SHAPE = new Tetromino(".....\n.....\nIIII.\n.....\n.....\n", 2);
 
     shape;
+    orientationShapes = [];
 
-    constructor(s) {
+    constructor(s, orientationCount=0) {
         this.shape = RotatingShape.fromString(s);
+        this.orientationShapes[0] = this.shape;
+        if (orientationCount > 0) {
+            for (let i = 1; i < orientationCount; i++) {
+                this.orientationShapes[i] = this.orientationShapes[i-1].rotateRight();
+            }
+        }
     }
 
     toString() {
